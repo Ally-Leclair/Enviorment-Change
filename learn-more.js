4cd36453b8576471898b938fe7889703a72900a1
Glitch (hello-webpage) <none>
2020-06-25 21:58:35 +0000

    🐁🛰 Checkpoint ./learn-more.js:17518768/1408 ./take-action.html:17626804/304 ./take-action.css:17626804/30 ./learn-more.html:17518768/151

--- learn-more.js
+++ learn-more.js
@@ -2,7 +2,8 @@ var CO2 = document.getElementById("CO2E")
   CO2.addEventListener("click",CO2function)
 
 function CO2function(){
-   window.scrollY + document.querySelector('#p1').getBoundingClientRect().top // Y
-
-window.scrollX + document.querySelector('p1').getBoundingClientRect().left // X
+ window.scroll(3000,605);
 }
+
+
+
