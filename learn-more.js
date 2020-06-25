a6428aed66f14de6dc11c8b984e2b2ede4d03967
Glitch (hello-webpage) <none>
2020-06-25 21:47:22 +0000

    🎭⛴ Checkpoint ./learn-more.css:17518768/1041 ./learn-more.js:17518768/235 ./learn-more.html:17518768/52

--- learn-more.js
+++ learn-more.js
@@ -2,5 +2,7 @@ var CO2 = document.getElementById("CO2E")
   CO2.addEventListener("click",CO2function)
 
 function CO2function(){
-   window.scroll(300, 300)
+   window.scrollY + document.querySelector('#p1').getBoundingClientRect().top // Y
+
+window.scrollX + document.querySelector('p1').getBoundingClientRect().left // X
 }
