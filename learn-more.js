e3a55736cbbd5a3890add97a6ff1419afb54ea43
Glitch (hello-webpage) <none>
2020-06-25 22:21:34 +0000

    🍃🌂 Checkpoint ./learn-more.js:17518768/297 ./take-action.html:17626804/93 ./learn-more.html:17518768/121 ./aboutus.html:17626804/158 ./aboutus.css:17626804/116

--- learn-more.js
+++ learn-more.js
@@ -1,8 +1,11 @@
-var CO2=document.getElementById("CO2E");
-var h1 =
-CO2.addEventListener("click",CO22)
+var CO2=document.getElementById("CO2E")
+ CO2.addEventListener("click",CO22)
+
+
+
 function CO22(clicked){
-  window.scrollTo(3000,640);
+var elmntToView = document.getElementById("p1");
+elmntToView.scrollIntoView(); 
 }
 
 
