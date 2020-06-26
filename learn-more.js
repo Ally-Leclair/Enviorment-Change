c97cd70287c706856e3e6c950538b97194245835
Glitch (hello-webpage) <none>
2020-06-26 04:49:31 +0000

    💷🎅 Checkpoint ./learn-more.css:17518768/2199 ./learn-more.html:17518768/7:17626804/245 ./learn-more.js:17518768/23:17626804/649 ./style.css:17518768/30 ./take-action.html:17626804/10 ./take-action.css:17626804/69

---
+++ learn-more.js
@@ -0,0 +1,27 @@
+/*Go to top*/
+var backtotop = document.getElementById("backtotop");
+  backtotop.addEventListener("click", topfunction);
+
+  function topfunction() {
+  location.reload();
+  }
+
+/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
+ /*Scroll to top when arrow up clicked END*/
+
